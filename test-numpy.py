d221615549b4c4722d1434af1984a2cdaba31aba
MatrixAwakens69 <ponnuponnanna94@gmail.com>
2024-02-03 14:46:51 +0530

    matplotlib

--- test-numpy.py
+++ test-numpy.py
@@ -1,1 +1,18 @@
-import numpy as np;
+import matplotlib.pyplot as plt
+import numpy as numpy
+import pandas as pd
+
+x=[1,2,3,4]
+y=[3,6,9,12]
+plt.plot(x,y,label='3x',color='red',linewidth=2,marker='.',markersize=10,markeredgecolor='black',linestyle='--')
+
+plt.title('Test Graph', fontdict={'fontname': 'Comic Sans MS', 'fontsize': 20})
+plt.xlabel('X Axis')
+plt.ylabel('Y Axis')
+
+plt.xticks([1,2,3,4])
+plt.yticks([3,6,9,12])
+
+plt.legend()
+
+plt.show()
