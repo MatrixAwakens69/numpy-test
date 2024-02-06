cc3e0894f128e0be3f72e5f2dbb493456cad59c2
MatrixAwakens69 <ponnuponnanna94@gmail.com>
2024-02-06 22:50:16 +0530

    finished line graphs

--- test-numpy.py
+++ test-numpy.py
@@ -1,18 +1,28 @@
 import matplotlib.pyplot as plt
-import numpy as numpy
+import numpy as np
 import pandas as pd
 
-x=[1,2,3,4]
-y=[3,6,9,12]
-plt.plot(x,y,label='3x',color='red',linewidth=2,marker='.',markersize=10,markeredgecolor='black',linestyle='--')
+x=[0,1,2,3,4]
+y=[0,3,6,9,12]
+
+plt.figure(figsize=(5,3), dpi=150)
+
+# plt.plot(x,y,label='3x',color='red',linewidth=2,marker='.',markersize=10,markeredgecolor='black',linestyle='--')
+plt.plot(x,y,'b^--',label='3x')
+
+x2 = np.arange(0,4.5,0.5)
+plt.plot(x2[:7],x2[:7]**2,'r',label='x^2')
+plt.plot(x2[6:],x2[6:]**2,'r--',label='x^2')
 
 plt.title('Test Graph', fontdict={'fontname': 'Comic Sans MS', 'fontsize': 20})
 plt.xlabel('X Axis')
 plt.ylabel('Y Axis')
 
-plt.xticks([1,2,3,4])
-plt.yticks([3,6,9,12])
+plt.xticks([0,1,2,3,4])
+# plt.yticks([3,6,9,12])
 
 plt.legend()
+
+plt.savefig('testgraph.png', dpi=300)
 
 plt.show()
